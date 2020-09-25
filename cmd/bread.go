7d14526ebf4c20cf02f139934f0c247112369387
Johnny <npcdja@mrblue.com>
2020-09-25 12:09:12 +0900

    feat: 빵, 전자제품 주문 시스템 커맨드 추가

--- cmd/bread.go
+++ cmd/bread.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var breadCmd = &cobra.Command{
+	Use:     "bread",
+	Short:   "빵 주문 시스템임",
+	Long:    "빵 주문 시스템입니다.",
+	Example: "bread --config test",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("빵 주문 시스템을 이용해주셔서 감사합니다.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(breadCmd)
+}
--- cmd/digital.go
+++ cmd/digital.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var digitalCmd = &cobra.Command{
+	Use:     "digital",
+	Short:   "전자제품 주문 시스템임",
+	Long:    "전자제품 주문 시스템입니다.",
+	Example: "digital --config test",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("전자제품 주문 시스템을 이용해주셔서 감사합니다.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(digitalCmd)
+}
