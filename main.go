5a56f9d82fae940dcde6a3731970aa32d2f05620
Johnny <npcdja@mrblue.com>
2020-09-25 11:17:15 +0900

    docs: main.go 실행 방법 주석 추가

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "cobra_sample/cmd"
 
+// 실행방법
+// 1. go run $project_folder/main.go [options]
+// 2. go run $project_folder/main.go Johnny [options]
 func main() {
 	cmd.Execute()
 }
