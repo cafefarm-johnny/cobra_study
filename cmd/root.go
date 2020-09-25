b9dc60cd7bfbecc99d9784dd670ff5d00cc0aa3a
Johnny <npcdja@mrblue.com>
2020-09-25 11:14:06 +0900

    feat: CLI 실행 함수 추가

---
+++ cmd/root.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "Johnny",
+	Short: "This is short comment.",
+	Long:  `This is long comment.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(cfgFile)
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// main.go 실행 시 cmd 패키지 안에 init()을 실행하여 커맨드를 파싱한다.
+// 커맨드 파싱 완료 후 Run에 정의된 함수를 호출한다.
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "(test)default config", "config file (default is $HOME/.cli.yaml)")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cli")
+	}
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+
+	if err == nil {
+		fmt.Println("Using Config File: ", viper.ConfigFileUsed())
+	}
+}
--- main.go
+++ main.go
@@ -1,4 +1,7 @@
 package main
 
+import "cobra_sample/cmd"
+
 func main() {
+	cmd.Execute()
 }
