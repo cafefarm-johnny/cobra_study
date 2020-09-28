2953575bc6621d750af9a1946631d7c7535b20f1
Johnny <npcdja@mrblue.com>
2020-09-28 10:37:48 +0900

    feat: 버전 정보 관리  추가

--- cmd/bread.go
+++ cmd/bread.go
@@ -13,9 +13,13 @@ var breadCmd = &cobra.Command{
 	Example: "bread --config test",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("빵 주문 시스템을 이용해주셔서 감사합니다.")
+		fmt.Printf("다음 제품을 구매하시는게 맞으신가요? \"%s\"\n", product)
 	},
 }
 
+var product string
+
 func init() {
 	rootCmd.AddCommand(breadCmd)
+	breadCmd.PersistentFlags().StringVar(&product, "product", "cream bread", "A help for product.")
 }
